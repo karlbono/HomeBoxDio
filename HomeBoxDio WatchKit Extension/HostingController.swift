927d1d581c83836155b48bd2f7446037b3e6006d
Karl Bono <karl@bono.be>
2020-01-07 15:04:11 +0100

    Initial Commit

---
+++ HomeBoxDio WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  HomeBoxDio WatchKit Extension
+//
+//  Created by Karl Bono on 07/01/2020.
+//  Copyright © 2020 Karl Bono. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ HomeBoxDio WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  HomeBoxDio WatchKit Extension
+//
+//  Created by Karl Bono on 07/01/2020.
+//  Copyright © 2020 Karl Bono. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
